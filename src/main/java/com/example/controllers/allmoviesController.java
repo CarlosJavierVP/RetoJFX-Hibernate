3b29505255e2a7dd22d78c569be73cd269f7cc62
CarlosJavier <carlos.valenzuela.j92@gmail.com>
2024-11-01 20:49:29 +0100

    Commit implementación search bar en la vista de allMovies

--- src/main/java/com/example/controllers/allmoviesController.java
+++ src/main/java/com/example/controllers/allmoviesController.java
@@ -5,6 +5,10 @@ import com.example.GestorApp;
 import com.example.HibernateUtil;
 import com.example.dao.PeliculaDAO;
 import com.example.models.Pelicula;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
@@ -17,15 +21,22 @@ public class allmoviesController implements Initializable {
     @javafx.fxml.FXML
     private TableView <Pelicula> tableMovies;
     @javafx.fxml.FXML
-    private TableColumn <String, String>tablaTitulo;
+    private TableColumn <Pelicula, String>tablaTitulo;
     @javafx.fxml.FXML
-    private TableColumn <String, String> tablaGenero;
+    private TableColumn <Pelicula, String> tablaGenero;
     @javafx.fxml.FXML
-    private TableColumn <String, String> tablaFecha;
+    private TableColumn <Pelicula, String> tablaFecha;
     @javafx.fxml.FXML
-    private TableColumn <String, String> tablaDirector;
+    private TableColumn <Pelicula, String> tablaDirector;
+    @javafx.fxml.FXML
+    private TextField search;
+    @javafx.fxml.FXML
+    private ChoiceBox searchChoiceBox;
     private List<Pelicula> allMovies;
     PeliculaDAO peliDAO = new PeliculaDAO(HibernateUtil.getSessionFactory());
+
+    ObservableList<Pelicula> filter = FXCollections.observableArrayList();
+    FilteredList<Pelicula> filterMovie = new FilteredList<>(filter, p -> true); //Inicializar la lista filtrada
 
     @Deprecated
     @Override
@@ -38,12 +49,48 @@ public class allmoviesController implements Initializable {
         allMovies = peliDAO.findAll();
         allMovies.forEach(peli ->{
             tableMovies.getItems().add(peli);
+            filter.add(peli);
         });
 
         tableMovies.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) ->{
             CurrentSession.movieSelected = newValue;
             GestorApp.loadFXML("views/detail-view.fxml","Movie Pro Manager - "+CurrentSession.userSelected.getNombreUsuario());
         });
+
+        search();
+
+
+    }
+
+    private void search() {
+        //filtrar por películas
+        tableMovies.setEditable(true);
+        tableMovies.setItems(filter);
+
+        search.textProperty().addListener((observable, oldValue, newValue) ->{
+            filterMovie.setPredicate(filter ->{
+                if (newValue.isEmpty() || newValue.isBlank()){
+                    return true;
+                }
+                String searchKey = newValue.toLowerCase();
+
+                if(filter.getTitulo().toLowerCase().contains(searchKey)){
+                    return true;
+                } else if (filter.getGenero().toLowerCase().contains(searchKey)) {
+                    return true;
+                } else if (filter.getAño().toString().toLowerCase().contains(searchKey)) {
+                    return true;
+                } else if (filter.getDirector().toLowerCase().contains(searchKey)) {
+                    return true;
+                }else {
+                    return false; //no hay coincidencias
+                }
+            });
+        });
+
+        SortedList<Pelicula> busquedaOrdenada = new SortedList<>(filterMovie);
+        busquedaOrdenada.comparatorProperty().bind(tableMovies.comparatorProperty()); //sincronización - la tabla muestra los datos sincronizados
+        tableMovies.setItems(busquedaOrdenada); //aplicar filtro con los datos ordenados en la tabla
     }
 
     @javafx.fxml.FXML
